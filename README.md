3f820a4878e2f845d9bc5e8934ddef469680fe05
Constantin Titarenko <constantin.titarenko@gmail.com>
2014-11-11 17:56:47 +0200

    Allow aliasing

--- README.md
+++ README.md
@@ -12,6 +12,12 @@ npm install joba-worque --save
 
 Connection object should contain same properties as it's required for worque module.
 
+Methods:
+
+### rename(task, alias)
+
+Allows to specify queue name for task with given name (by default queue name is the same as task name).
+
 ## License
 
 BSD
--- index.js
+++ index.js
@@ -1,5 +1,22 @@
 var worque = require('worque');
 
+function Adapter (config) {
+	this.client = new worque.Client(config);
+	this.aliases = {};
+}
+
+Adapter.prototype.subscribe = function (name, hander) {
+	return this.client.subscribe(this.aliases[name] || name, hander);
+};
+
+Adapter.prototype.publish = function (name, message) {
+	return this.client.publish(this.aliases[name] || name, message);
+};
+
+Adapter.prototype.rename = function (original, alias) {
+	this.aliases[original] = alias;
+};
+
 module.exports = function build (config) {
-	return new worque.Client(config);
+	return new Adapter(config);
 };
